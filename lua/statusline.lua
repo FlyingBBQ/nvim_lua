fa88328770917647d6ed7efccbb7b1ee46b4edb0
FlyingBBQ <derek.cardol@gmail.com>
2021-11-19 23:04:48 +0100

    feat: add simple statusline

---
+++ lua/statusline.lua
@@ -0,0 +1,28 @@
+local function nr_of_buffers()
+    return '[' .. #(vim.api.nvim_list_bufs()) .. ']'
+end
+
+local function get_path()
+    local path = string.match(vim.api.nvim_buf_get_name(0), "^(.-)[^/]-$")
+    local relative = string.match(path, "/home/.-(/.-)$")
+    if relative and ((vim.api.nvim_win_get_width(0) - 40) > (3 * #relative)) then
+        return '~' .. relative
+    else
+        return ''
+    end
+end
+
+function status_line()
+    return table.concat {
+        nr_of_buffers(),
+        " :: %t :: ",
+        "%h%m%r",
+        "%=%<", -- center
+        get_path(),
+        "%=", -- right side
+        "%3.p%% ",
+        "[%3l/%L ::%3.c]",
+    }
+end
+
+vim.o.statusline = "%!luaeval('status_line()')"
