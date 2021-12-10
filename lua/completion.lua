500c88a2709703dbb46487888f504ec975af0dfb
FlyingBBQ <derek.cardol@gmail.com>
2021-12-10 21:50:17 +0100

    feat(lsp): add lua language server

--- lua/completion.lua
+++ lua/completion.lua
@@ -56,10 +56,38 @@ nvim_lsp.clangd.setup {
         "clangd",
         "--background-index",
         "--clang-tidy",
-        "--query-driver=/usr/bin/arm-none-eabi-gcc,/usr/bin/**/clang-*",
+        -- Custom compilers
+        -- "--query-driver=/usr/bin/arm-none-eabi-gcc,/usr/bin/**/clang-*",
     },
     on_attach = on_attach,
     capabilities = capabilities,
+}
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+nvim_lsp.sumneko_lua.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
 
 -- Setup for nvim-cmp (completion).
