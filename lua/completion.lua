ea0e49db6648c7c994aa1abe897df1e5348d26ad
FlyingBBQ <derek.cardol@gmail.com>
2022-11-13 21:48:11 +0100

    fix: remove buffer completion

--- lua/completion.lua
+++ lua/completion.lua
@@ -148,7 +148,6 @@ cmp.setup {
         end,
     },
     sources = {
-        { name = 'buffer' },
         { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
@@ -156,10 +155,3 @@ cmp.setup {
     },
 }
 
--- Use buffer source for `/`.
-cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,7 +33,6 @@ return require('packer').startup(function()
     use {
         'hrsh7th/nvim-cmp',
         requires = {
-            'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-nvim-lsp',
