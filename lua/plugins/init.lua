8fc913d6b0774469866f11cbc0435fc9efeea7f5
FlyingBBQ <derek.cardol@gmail.com>
2023-02-25 00:26:48 +0100

    feat: move all plugins to lazy.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,20 +31,20 @@ return {
         event = { 'BufReadPost', 'BufNewFile' },
         opts = {
             ensure_installed = {
-                "bash",
-                "c",
-                "comment",
-                "css",
-                "go",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "python",
-                "rst",
-                "rust",
-                "svelte",
-                "yaml",
+                'bash',
+                'c',
+                'comment',
+                'css',
+                'go',
+                'html',
+                'javascript',
+                'json',
+                'lua',
+                'python',
+                'rst',
+                'rust',
+                'svelte',
+                'yaml',
             },
             highlight = {
                 enable = true,
@@ -52,17 +52,20 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
+                    init_selection = 'gnn',
+                    node_incremental = 'grn',
+                    scope_incremental = 'grc',
+                    node_decremental = 'grm',
                 },
             },
             indent = {
                 enable = true,
-                disable = { "c", "yaml" },
+                disable = { 'c', 'yaml' },
             }
         },
+        config = function(_, opts)
+            require('nvim-treesitter.configs').setup(opts)
+        end,
     },
 
     -- Motion
@@ -127,8 +130,8 @@ return {
                 border = true,
                 borderchars = {
                     { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-                    prompt = {"─", "│", " ", "│", "┌", "┐", "│", "│"},
-                    results = {"─", "│", "─", "│", "├", "┤", "┘", "└"},
+                    prompt = {'─', '│', ' ', '│', '┌', '┐', '│', '│'},
+                    results = {'─', '│', '─', '│', '├', '┤', '┘', '└'},
                     preview = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
                 },
             },
@@ -177,6 +180,229 @@ return {
                 untracked = { hl = 'CusorLineNR', text = '┆' },
             },
         },
+    },
+
+    -- Autocomplete
+    {
+        'neovim/nvim-lspconfig',
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        },
+        config = function()
+            -- Automatically install some language servers using mason.
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'bashls',
+                    'clangd',
+                    'lua_ls',
+                    'pyright',
+                    'rust_analyzer',
+                }
+            })
+
+            -- Use the autocommand 'LspAttach' as on_attach for each server.
+            local lsp_cmds = vim.api.nvim_create_augroup('lsp_cmds', {clear = true})
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = lsp_cmds,
+                desc = 'My global on_attach',
+                callback = function(args)
+                    local bufnr = args.buf
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+                    -- Mappings.
+                    local opts = { buffer = bufnr, silent = true }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set('n', '<space>n', vim.lsp.buf.rename, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+
+                    if client.server_capabilities.completionProvider then
+                        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+                    end
+
+                    -- Set autocommands conditional on server_capabilities.
+                    if client.server_capabilities.document_highlight then
+                        vim.api.nvim_set_hl(0, 'LspReferenceRead', { bg = '#3A3A3A' })
+                        vim.api.nvim_set_hl(0, 'LspReferenceText', { bg = '#3A3A3A' })
+                        vim.api.nvim_set_hl(0, 'LspReferenceWrite', { bg = '#3A3A3A' })
+
+                        local document_hightlight_group = vim.api.nvim_create_augroup(
+                            'LspDocumentHighlight', { clear = true }
+                        )
+                        vim.api.nvim_create_autocmd("CursorHold", {
+                            buffer = 0,
+                            callback = vim.lsp.buf.document_highlight,
+                            group = document_hightlight_group,
+                        })
+                        vim.api.nvim_create_autocmd("CursorMoved", {
+                            buffer = 0,
+                            callback = vim.lsp.buf.clear_references,
+                            group = document_hightlight_group,
+                        })
+                    end
+                end
+            })
+
+            local lspconfig = require('lspconfig')
+            local lsp_defaults = lspconfig.util.default_config
+
+            -- Add client capabilities to the default LSP configuration.
+            lsp_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+            )
+
+            -- Configure LSP servers through default or custom handlers.
+            require('mason-lspconfig').setup_handlers({
+                -- The first entry (without a key) will be the default handler
+                -- and will be called for each installed server that doesn't have
+                -- a dedicated handler.
+                function (server_name) -- default handler (optional)
+                    lspconfig[server_name].setup {}
+                end,
+
+                -- Next, you can provide targeted overrides for specific servers.
+                ['clangd'] = function ()
+                    lspconfig.clangd.setup {
+                        cmd = {
+                            'clangd',
+                            '--background-index',
+                            '--clang-tidy',
+                            -- Custom compilers
+                            -- '--query-driver=/usr/bin/arm-none-eabi-gcc,/usr/bin/**/clang-*',
+                        },
+                    }
+                end,
+                ['lua_ls'] = function ()
+                    local runtime_path = vim.split(package.path, ';')
+                    table.insert(runtime_path, 'lua/?.lua')
+                    table.insert(runtime_path, 'lua/?/init.lua')
+                    lspconfig.lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                                    version = 'LuaJIT',
+                                    -- Setup your lua path
+                                    path = runtime_path,
+                                },
+                                diagnostics = {
+                                    -- Get the language server to recognize the `vim` global
+                                    globals = {'vim'},
+                                },
+                                workspace = {
+                                    -- Make the server aware of Neovim runtime files
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                telemetry = {
+                                    enable = false,
+                                },
+                            },
+                        },
+                    }
+                end,
+            })
+
+        end,
+    },
+
+    {
+        'L3MON4D3/LuaSnip',
+        version = '<CurrentMajor>.*',
+        dependencies = {
+            'rafamadriz/friendly-snippets',
+            config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+        },
+    },
+
+    {
+        'hrsh7th/nvim-cmp',
+        version = false, -- last release is way too old
+        event = 'InsertEnter',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        opts = function()
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+            return {
+                completion = {
+                    completeopt = 'menu,menuone,noinsert',
+                },
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-e>'] = cmp.mapping.close(),
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<C-k>'] = cmp.mapping(function(fallback)
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        elseif cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-j>'] = cmp.mapping(function(fallback)
+                        if luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                },
+                sources = {
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'nvim_lsp' },
+                    { name = 'nvim_lua' },
+                    { name = 'luasnip' },
+                },
+            }
+        end,
     },
 
 }
