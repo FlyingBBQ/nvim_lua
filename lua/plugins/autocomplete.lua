d94f21e147d7a8cf586e73713df9d6e649b1fb4b
FlyingBBQ <derek.cardol@gmail.com>
2023-02-28 20:01:42 +0100

    fix: luasnip version

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,7 +1,7 @@
 return {
     {
         'L3MON4D3/LuaSnip',
-        version = '<CurrentMajor>.*',
+        version = 'v1.*',
         dependencies = {
             'rafamadriz/friendly-snippets',
             config = function()
