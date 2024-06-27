9df201a76e81c5ef1a001788c0dea2c9ab09d981
FlyingBBQ <derek.cardol@gmail.com>
2024-06-27 21:29:11 +0200

    feat: bump telescope to v1.8

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,13 +1,13 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.4',
+        tag = '0.1.8',
         dependencies = {
             { 'nvim-lua/plenary.nvim' },
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         },
         keys = {
-            {'<C-p>', '<cmd>Telescope find_files<cr>' },
+            { '<C-p>', '<cmd>Telescope find_files<cr>' },
             { '<leader>b', '<cmd>Telescope buffers<cr>' },
             { '<Leader>m', '<cmd>Telescope git_files<cr>' },
             { '<leader>g', '<cmd>Telescope live_grep<cr>' },
